9f81f0e1336c648dd43e0baae419a8afbe7626fa
Scott Ivey <scott.ivey@gmail.com>
2017-12-04 02:21:47 -0500

    yolo

--- src/lib/3000-0000-util.zsh
+++ src/lib/3000-0000-util.zsh
@@ -1,9 +1,45 @@
 #!/usr/bin/env zsh
 
-zinve::sha256() { sha256sum $1 | cut -d ' ' -f 1 ; }
+
+
+zinve::nth() { cut -d ' ' -f $1 ; }
+zinve::digest::sha256() { sha256sum $1 | zinve::nth 1 ; }
+
+zinve::noop ${ZINVE__XXHASH_BASENAME:=xxhsum}
+zinve::digest::xxhash() { $ZINVE__XXHASH_BASENAME $1 | zinve::nth 1 ; }
+
+zinve::digest::have-xxhash() {
+    if ! whence -p $ZINVE__XXHASH_BASENAME &>/dev/null; then
+        return 1
+    fi
+}
+
+zinve::digest::choose-fn-and-suffix() {
+    local fname='zinve::digest'
+    local suffix='.asc'
+
+    if zinve::digest::have-xxhash ; then
+        fname+='::xxhash'
+        suffix=".xxhash${suffix}"
+    else
+        fname+='::sha256'
+        suffix=".sha256${suffix}"
+    fi
+    echo $fname $suffix ;
+
+}
+
+
 zinve::has-key() { [[ "${${(P)1}[$2]:-""}" != "" ]] || return 1 ; }
 zinve::isdef() { [[ ! -z ${${(P)1}+x} ]] || return 1 ; }
-
+zinve::has-any-key() {
+    local aname=$1 ; shift ;
+    local kname ;
+    for kname in $@; do
+        if zinve::has-key $aname $kname; then return ; fi
+    done
+    return 1
+}
 zinve::key-coalesce() {
     local name=$1 ; shift ;
     local keyk ;
--- src/lib/7000-0000-venv.zsh
+++ src/lib/7000-0000-venv.zsh
@@ -43,46 +43,74 @@ _zinve::venv::get-version-from-bin-dir() {
 _zinve::venv::find-wanted-python-version() {
     local pybin=$1 ; shift ;
     local pybase=${pybin:t}
+    local failmsg="$0 - don't know what to do with '$pybase' ('$pybin')"
     if [[ $pybase != python* ]]; then
-        zinve::fatal "$0 - don't know what to do with '$pybase' ('$pybin')"
+        zinve::fatal $failmsg ; return 1 ;
     fi
     if [[ $pybase =~ ^python[0-9].* ]]; then
-        echo ${pybase##python}
-    else
-        local bin_d=""
-        if [[ ${pybin} == '/'* ]]; then
-            bin_d=$pybin
-        else
-            read -r bin_d < <( whence -p $pybase ) ;
-            bin_d=${bin_d:h}
-        fi
-        _zinve::venv::get-version-from-bin-dir $bin_d ;
+        echo ${pybase##python} ; return ;
     fi
+    if [[ $pybase != 'python' ]]; then
+        zinve::fatal $failmsg ; return 1 ;
+    fi
+    local bin_d=""
+    if [[ ${pybin} == '/'* ]]; then
+        bin_d=${pybin:h}
+    else
+        read -r bin_d < <( whence -p $pybase ) ;
+        bin_d=${bin_d:h}
+    fi
+    # since the user didn't specify a point version, we just match
+    # on major
+    _zinve::venv::get-version-from-bin-dir $bin_d \
+        | sed -r 's/\..*//'
 }
 
 zinve::venv::ensure-by-path() {
-    typeset -A optmap
     typeset -a reqs=()
     typeset -a adef=()
-    zparseopts -a adef -A optmap r+:=reqs \
-            p: -python: d: -venv-dir: \
-            f -force
+    typeset -a force_list=()
+    typeset -a pybin_list=()
+    typeset -a dir_list=()
+    zparseopts -a adef r+:=reqs \
+            f=force_list -force=force_list \
+            p:=pybin_list -python:=pybin_list \
+            d:=dir_list -venv-dir:=dir_list
 
     local venv_d=""
-    read -r venv_d < <( zinve::key-coalesce optmap '-d' '--venv-dir' )
-    local pybin=""
-    if ! read -r pybin < <( zinve::key-coalesce optmap '-p' '--python' ); then
+    local kcfn='zinve::key-coalesce'
+    if [[ ${#dir_list} -ge 2 ]]; then
+        venv_d=${dir_list[2]}
+    else
+        zinve::fatal "Specify target directory with -d / --venv-dir." ;
+    fi
+    if [[ ${#pybin_list} -ge 2 ]]; then
+        pybin=${pybin_list[2]}
+    else
         pybin='python'
     fi
-    local is_force=false ;
     typeset -a bad_elems=( '-r' )
     reqs=( ${reqs:|bad_elems} )
+
+    local is_force=false ;
+    if [[ ${#force_list} -gt 0 ]]; then
+        is_force=true ;
+    fi
+
     local venv_z_state_d=$venv_d/.zinve
     local upit=false
     local bin_d=$venv_d/bin
     local msg=""
     if [[ -d $venv_d ]]; then
-        if [[ -d $bin_d ]]; then
+        # if the venv dir exists, we want to make sure that:
+        #   - it's actually a virtualenv.
+        #   - its python version matches the one specified
+        #     by the -p / --python CLI flag.
+        #
+        # currently the first check is handled by looking for
+        # $venv_d/bin/python.
+        #
+        if [[ -d $bin_d ]] && [[ -e $bin_d/python ]] then
             local wanted_v="" ;
             read -r wanted_v < <( {
                 _zinve::venv::find-wanted-python-version $pybin
@@ -106,21 +134,22 @@ zinve::venv::ensure-by-path() {
             else
                 msg="python version '$current_v' in '$venv_d' does not"
                 msg+=" match target version '$wanted_v'."
-                vinve::warn $msg ;
+                zinve::warn $msg ;
                 msg=""
                 if ( $is_force ); then
-                    msg="Because --force is enabled, I'm killing $venv_d"
+                    msg="Because -f (force) is enabled, I'm killing $venv_d"
                     msg+=" and rebuilding."
                     zinve::warn $msg ;
                     rm -rf $venv_d ;
                     zinve::warn "( removed $venv_d )"
                 else
                     msg="Aborting. If you want me to rebuild $venv_d with"
-                    msg+=" version '$wanted_v', pass the --force flag."
+                    msg+=" version '$wanted_v', pass the -f (force) flag."
                     zinve::fatal "$msg" ; return 1 ;
                 fi
             fi
         else
+            # $venv_d/bin/python does not exist.
             zinve::warn "'$venv_d' doesn't seem to be a valid virtualenv."
             if zinve::try-rmdir $venv_d ; then
                 msg="Target '$venv_d' existed but wasn't a virtualenv."
@@ -128,40 +157,41 @@ zinve::venv::ensure-by-path() {
                 zinve::warn $msg ;
             else
                 if ( $is_force ); then
-                    msg="Existing target '$venv_d' is invalid and --force"
+                    msg="Existing target '$venv_d' is invalid and -f (force)"
                     msg+=" is enabled: removing '$venv_d' and rebuilding."
                     zinve::warn $msg ;
                     rm -rf $venv_d ;
                 else
                     msg="Target dir '$venv_d' exists and is non-empty, but it"
                     msg+=" doesn't look like a virtualenv dir."
-                    msg+=" If you want me to fix it, pass --force."
+                    msg+=" If you want me to fix it, pass -f (force)."
                     zinve::fatal $msg ; return 1 ;
                 fi
             fi
         fi
     fi
 
-    if [[ -d $venv_d ]]; then
-        true
-    else
+    if [[ ! -d $venv_d ]]; then
         mkdir -p ${venv_d:h} ;
         virtualenv -p ${pybin} $venv_d ;
         ${venv_d}/bin/pip install --upgrade pip
         upit=true
     fi
+
     local dig_d=$venv_z_state_d/digest
     local dig_tmp_d=$venv_z_state_d/tmp/digest/
     mkdir -p $dig_d $dig_tmp_d;
     typeset -a needed_req_files=()
     typeset -A output_digests; output_digests=() ;
     local curr_reqf
+    local dig_fn dig_suffix
+    read -r dig_fn dig_suffix < <( zinve::digest::choose-fn-and-suffix )
     for curr_reqf in ${reqs[@]}; do
         curr_reqf=${curr_reqf:A}
         local dig_key;
         read -r dig_key < <( zinve::make-fs-cache-key $curr_reqf ) ;
-        local dig_f="${dig_d}/$dig_key.sha256.asc"
-        local curr_dig; read -r curr_dig < <( zinve::sha256 $curr_reqf )
+        local dig_f="${dig_d}/${dig_key}${dig_suffix}"
+        local curr_dig; read -r curr_dig < <( $dig_fn $curr_reqf )
         curr_dig="${curr_dig// /}"
         local prev_dig=""
         if [[ -e $dig_f ]]; then
@@ -175,6 +205,8 @@ zinve::venv::ensure-by-path() {
         output_digests+=( $dig_f $curr_dig )
         needed_req_files+=( $curr_reqf )
     done
+
+    # none of the requirements digests have changed.
     if (! $upit); then return 0 ; fi
 
     if [[ ${#needed_req_files} -gt 0 ]]; then
@@ -200,10 +232,13 @@ _zinve::venv::run-or-exec-in-venv() {
     local zinve_cmd_name=${1:u} ; shift ;
     zinve::venv::ensure-by-path $@ || zinve::fatal "venv check failed" ;
     typeset -a nothing=()
+    typeset -a dir_list=() ;
     typeset -A optmap2 ; optmap2=() ;
-    zparseopts -D -E -a nothing -A optmap2 d: -venv-dir:
+    zparseopts -D -E -a nothing d:=dir_list -venv-dir:=dir_list
+
     local venv_d=""
-    read -r venv_d < <( zinve::key-coalesce optmap2 '-d' '--venv-dir' )
+    [[ ${#dir_list} -ge 2 ]] || zinve::fatal "$0 unspecified venv dir" ;
+    venv_d=${dir_list[2]}
 
     typeset -a cmd_parts=() ; local curr ;
     while [[ $# -gt 0 ]]; do
